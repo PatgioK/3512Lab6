25cacf9b0f30767ad9362ebfd2fa7b90bd13bbbf
Patgio Kwok <Patgiok@gmail.com>
2018-11-02 13:20:14 -0700

    dictionary class

---
+++ dictionary.cpp
@@ -0,0 +1,5 @@
+//
+// Created by patgi on 2018-11-02.
+//
+
+#include "dictionary.hpp"
---
+++ dictionary.hpp
@@ -0,0 +1,14 @@
+//
+// Created by patgi on 2018-11-02.
+//
+
+#ifndef INC_3512LAB6_DICTIONARY_HPP
+#define INC_3512LAB6_DICTIONARY_HPP
+
+
+class dictionary {
+
+};
+
+
+#endif //INC_3512LAB6_DICTIONARY_HPP
